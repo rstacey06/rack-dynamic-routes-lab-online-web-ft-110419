ea39cde21fd412acab04fa6bceb2d2912f404d48
github username <github email address>
2019-12-30 09:58:39 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application
+
+  @@items = []
+
+  def call(environment)
+    resp = Rack::Response.new
+    req = Rack::Request.new(environment)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item = @@items.find  {|i| i.name == item_name}
+        resp.write item.price
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
+    resp.finish
+  end
+end
